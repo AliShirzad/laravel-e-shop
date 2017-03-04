19db89e462f292b8f7e97b1461767f49dd58335a
alish <Befree.at1@gmail.com>
2017-03-04 15:04:01 +0330

    14 Esfand

--- app/Http/Controllers/addProduct.php
+++ app/Http/Controllers/addProduct.php
@@ -2,41 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
 
 class addProduct extends Controller
 {
 
-    public function addbaskets(Request $request, $id)
+    public function addbaskets($id)
     {
-        if(\Session::has('products'))
-        {
-           $products=\Session::get('products');
-            array_push($products,$id);
-        }
-        else{
-            $products=array(
-              $id,
+        if (\Session::has('products')) {
+            $products = \Session::get('products');
+            array_push($products, $id);
+        } else {
+            $products = array(
+                $id,
             );
         }
-        \Session::put('products',$products);
-        $data = \Session::get('products');
-        $data = array_unique($data);
-        $result=array();
-        foreach ($data as $dd){
-            $result[]=Product::find($dd);
-        }
-        return view('main.basket')->with('result',$result);
+        \Session::put('products', $products);
+
+        return redirect('/show');
     }
 
-    public function removebaskets(Request $request, $id){
-        if(\Session::has('products'))
-        {
-            $products=\Session::get('products');
-            array_pop($products, $id);
+    public function removebaskets($id)
+    {
+        $products = \Session::get('products');
+        $data = array_unique($products);
+        if(($key = array_search($id, $data)) !== false) {
+            unset($data[$key]);
         }
+        \Session::forget('products');
+        \Session::put('products',$data);
+        return redirect('/');
     }
 }
+
